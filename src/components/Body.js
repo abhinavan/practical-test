e7bf58832e4dbbfa5dc5f02827c454873073c02b
Abhinav Anand <abhinavan@Abhinavs-MacBook-Air.local>
2019-09-08 14:31:16 +0530

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Body extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>This is a body</p>
+      </div>
+    );
+  }
+}
+export default Body;
