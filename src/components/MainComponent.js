915664731aecdd69a93cadff467398ed5174009b
Abhinav Anand <abhinavan@Abhinavs-MacBook-Air.local>
2019-09-14 18:32:49 +0530

    changed App to MainComponent

---
+++ src/components/MainComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import ReactDom from "react-dom";
+import Body from "./Body";
+import Header from "./Header";
+
+function MainComponent() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Header />
+    </div>
+  );
+}
+export default MainComponent;
