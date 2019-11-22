a21ab1c324b681b4acfe0f052988dca9017e0170
abhinavan <abhinav.15oct1993@gmail.com>
2019-11-22 15:28:08 +0530

    time calculator added

--- src/components/timeCalculator.js
+++ src/components/timeCalculator.js
@@ -3,18 +3,25 @@ import React from "react";
 function TimeCalculator() {
   const date = new Date();
   const hrs = date.getHours();
+  const styles = {
+    fontSize: 100,
+    fontFamily: "Georgia"
+  };
   let timeOfDay;
 
   if (hrs >= 0 && hrs < 12) {
     timeOfDay = "Morning";
+    styles.color = "#66ccff";
   } else if (hrs > 12 && hrs < 18) {
     timeOfDay = "Afternoon";
+    styles.color = "#ff0000";
   } else {
     timeOfDay = "Evening";
+    styles.color = "#66ff33";
   }
 
   return (
-    <h1>
+    <h1 style={styles}>
       Good {timeOfDay} !!! Time of Day {date.getHours() % 12} o' clock
     </h1>
   );
